f020dc9ec81657a52425d0069e0cb67ddc555c59
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-08-20 19:27:26 +0900

    fix: css 라이브러리 emotion으로 변경

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,6 +16,23 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['react-app', { flow: false, typescript: true }],
+          require.resolve('@emotion/babel-preset-css-prop'),
+        ],
+      },
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
+    };
     return config;
   },
 }
