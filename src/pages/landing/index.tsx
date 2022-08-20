f8510e491566bcbc5e2d1ed16ba58bf2ebc5cc0d
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-08-20 19:28:09 +0900

    add: 아이콘 추가

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,16 +1,14 @@
-import className from 'classnames/bind';
 import React from 'react';
 
 import style from './style.module.scss';
 
-const cx = className.bind(style);
+import Header from '~/components/header/Header';
 
 export default function Index() {
     return (
-        <div className={cx('wrapper')}>
+        <div>
+            <Header />
             Landing
-            <p className={cx('test1')}>Test1 이거는 테스트</p>
-            <p className={cx('normal')}>Test1 이거는 테스트</p>
             <p>Test3</p>
             <p>Test4</p>
         </div>
