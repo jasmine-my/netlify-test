e86b1b779cdfe597a4e22184278ee1b3073ee41e
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-08-21 21:33:08 +0900

    add: Header #1

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,39 +1,74 @@
-import React from 'react';
+import { css } from '@emotion/react';
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
-import { headerStyle } from '~/components/header/style';
+import { headerStyle, snsIconStyle } from '~/components/header/style';
 import Icon from '~/components/icon/Icon';
+import { mediaQuery } from '~/global_styles/global';
 
 export default function Header() {
+    const isPC = useMediaQuery({ query: mediaQuery.PC });
+    const isTable = useMediaQuery({ query: mediaQuery.TABLET });
+    const isMobile = useMediaQuery({ query: mediaQuery.MOBILE });
+
+    const [isOpenedMenu, setIsOpenedMenu] = useState(false);
+
     return (
-        <div css={headerStyle}>
+        <div css={headerStyle(isOpenedMenu, isTable, isMobile)}>
             <div className={'wrap'}>
                 <img
                     className={'logo'}
                     src="/assets/images/logo-header.svg"
                     alt="header-logo"
                 />
-                <div className={'contents'}>
-                    <ul className={'gnbItems'}>
-                        <li>Story</li>
-                        <li>Collection</li>
-                        <li>Roadmap</li>
-                        <li>Benefit</li>
-                        <li>Team</li>
-                        <li>Partner</li>
-                    </ul>
-                    <ul className={'snsIcons'}>
-                        <li>
-                            <Icon size={32} name={'sns-instagram'} />
-                        </li>
-                        <li>
-                            <Icon size={32} name={'sns-discord'} />
-                        </li>
-                        <li>
-                            <Icon size={32} name={'sns-twitter'} />
-                        </li>
-                    </ul>
-                </div>
+
+                {isPC && (
+                    <div className={'contents'}>
+                        <ul className={'gnbItems'}>
+                            <li>Story</li>
+                            <li>Collection</li>
+                            <li>Roadmap</li>
+                            <li>Benefit</li>
+                            <li>Team</li>
+                            <li>Partner</li>
+                        </ul>
+                        <ul css={snsIconStyle}>
+                            <li>
+                                <Icon size={32} name={'sns-instagram'} />
+                            </li>
+                            <li>
+                                <Icon size={32} name={'sns-discord'} />
+                            </li>
+                            <li>
+                                <Icon size={32} name={'sns-twitter'} />
+                            </li>
+                        </ul>
+                    </div>
+                )}
+
+                {(isTable || isMobile) && (
+                    <Icon
+                        css={css`
+                            cursor: pointer;
+                        `}
+                        name={
+                            isOpenedMenu ? 'hamburger-close' : 'hamburger-open'
+                        }
+                        size={isTable ? 72 : 36}
+                        onClick={() => setIsOpenedMenu(!isOpenedMenu)}
+                    />
+                )}
             </div>
+            {(isTable || isMobile) && isOpenedMenu && (
+                <ul className={`gnbItems small`}>
+                    <li>Story</li>
+                    <li>Collection</li>
+                    <li>Roadmap</li>
+                    <li>Benefit</li>
+                    <li>Team</li>
+                    <li>Partner</li>
+                </ul>
+            )}
         </div>
     );
 }
---
+++ src/components/header/style.ts
@@ -0,0 +1,75 @@
+import { css } from '@emotion/react';
+
+import { font, pcMinWidth } from '~/global_styles/global';
+
+export const headerStyle = (
+    isOpenedMenu: boolean,
+    isTablet: boolean,
+    isMobile: boolean
+) => css`
+    background-color: ${isOpenedMenu
+        ? 'var(--BASIC-WHITE)'
+        : 'var(--OPACITY-WHITE)'};
+    width: 100%;
+    margin: 0 auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: all 0.3s;
+    .wrap {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: ${isTablet || isMobile ? 'auto' : pcMinWidth};
+        margin: 0 auto;
+        height: 120px;
+        padding: ${isTablet || isMobile ? '0 16px' : '0'};
+    }
+    .logo {
+        width: ${isMobile ? '104px' : '168px'};
+    }
+    .contents {
+        display: flex;
+        align-items: center;
+        gap: 63px;
+    }
+    .gnbItems {
+        display: flex;
+        gap: 64px;
+        ${font(
+            'Inter',
+            700,
+            18,
+            21.78,
+            `${
+                isTablet || isMobile
+                    ? 'var(--BASIC-WHITE)'
+                    : 'var(--BASIC-NAVY)'
+            }`
+        )};
+        li {
+            :hover {
+                cursor: pointer;
+                color: var(--SOW-GREEN);
+                transition: color 0.2s ease-in-out;
+            }
+        }
+        &.small {
+            background: var(--SOW-PURPLE);
+            padding: 61px 0 61px 55px;
+            flex-direction: column;
+        }
+    }
+`;
+
+export const snsIconStyle = css`
+    display: flex;
+    gap: 10px;
+    li {
+        cursor: pointer;
+        :hover {
+            transform: translateY(-5px);
+            transition: all 0.2s ease-in;
+        }
+    }
+`;
