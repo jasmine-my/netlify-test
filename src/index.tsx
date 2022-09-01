95f34d4d5dd8003682ebe52f9336bb9286b2ef89
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-09-01 18:20:50 +0900

    fix: 리액트 버전 18 에서 17로 다운그레이드 #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import { render } from 'react-dom';
 import './index.css';
 import '~/global_styles/font.css';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
+const container = document.getElementById('root');
+render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
+    container
 );
