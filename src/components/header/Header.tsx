4b39fd26b45e02239a70c8c987846dcc538e5174
ost006 <analysthj@gmail.com>
2022-10-03 13:47:40 +0900

    사파리 헤더 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { Link } from 'react-scroll';
 
@@ -8,7 +8,12 @@ import Icon from '~/components/icon/Icon';
 import { mediaQuery } from '~/global_styles/global';
 import HeaderLogo from '~/images/logo-header.svg';
 import { urlInfo } from '~/information/urlInfo';
-import useGetScrollPosition from '~/useHooks/useGetScrollPosition';
+
+/*
+스크롤 위치가 변경될 때 마다 state가 업데이트 되면 cpu에 불필요한 비용(부담)이 많이 발생하게 됩니다
+lastScrollPosition의 경우 JSX 부분과 관련이 없기 때문에 일반 변수로 선언하여 사용하는 것이 더 낫습니다
+*/
+let lastScrollPosition = 0;
 
 export default function Header() {
     const isPC = useMediaQuery({ query: mediaQuery.PC });
@@ -16,16 +21,27 @@ export default function Header() {
     const isMobile = useMediaQuery({ query: mediaQuery.MOBILE });
 
     const [isOpenedMenu, setIsOpenedMenu] = useState(false);
-    const [lastScrollPosition, setLastScrollPosition] = useState(0);
-    const [recentScrollPosition, setRecentScrollPosition] = useState(0);
-    const { scrollPosition } = useGetScrollPosition();
+    const [showHeader, setShowHeader] = useState(true);
+
+    const handleScroll = useCallback(() => {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition > 0) setIsOpenedMenu(false);
+        if (scrollPosition < 300) {
+            setShowHeader(true);
+        } else if (scrollPosition < lastScrollPosition) {
+            setShowHeader(true);
+        } else {
+            setShowHeader(false);
+        }
+        lastScrollPosition = window.scrollY;
+    }, []);
 
     useEffect(() => {
-        if (scrollPosition > 0) setIsOpenedMenu(false);
-
-        setLastScrollPosition(recentScrollPosition);
-        setRecentScrollPosition(scrollPosition);
-    }, [scrollPosition]);
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
 
     const menus = [
         { id: 'story', title: 'Story' },
@@ -37,15 +53,7 @@ export default function Header() {
     ];
     return (
         <div css={headerStyle(isOpenedMenu, isTable, isMobile)}>
-            <div
-                className={`wrap ${
-                    recentScrollPosition > lastScrollPosition
-                        ? 'scrollDown'
-                        : scrollPosition > 15
-                        ? 'top'
-                        : 'scrollUp'
-                }`}
-            >
+            <div className={`wrap ${showHeader ? 'scrollUp' : 'scrollDown'}`}>
                 <Link key={'main'} to={'main'} spy={true} smooth={true}>
                     <img
                         className={'logo'}
